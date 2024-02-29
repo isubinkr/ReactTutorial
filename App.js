565dc6024932d2d3b44a471c09a31f61ca7a3c6e
Subin Kumar <isubinkr@icloud.com>
2024-02-29 15:05:32 +0530

    After Episode-03

--- App.js
+++ App.js
@@ -1,24 +1,52 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  [React.createElement(
-    "div",
-    { id: "child" },
-    [React.createElement("h1", {}, "This is React Tutorial"),
-    React.createElement("h2", {}, "I'm a h2 tag")]
-  ),
-  React.createElement(
-    "div",
-    { id: "child" },
-    [React.createElement("h1", {}, "I'm a h1 tag"),
-    React.createElement("h2", {}, "I'm a h2 tag")]
-  )
-  ]
+// React Element
+
+const heading = React.createElement("h1", {id: "heading"}, "React tutorial") // heading is an object
+// this is react core but we don't use this instead we use jsx
+
+//when we render this to dom then it becomes a html element;
+
+// JSX - is not HTML in JS it is HTML-like or XML-like syntax
+// JSX is note javascript it won't work in console it is transpiled before it reaches the JS Engine - 
+// this is done by PARCEL - Babel (JSX -> Rect.createElement)
+// JSX => React.createElement => React Element - JS Object => HTML Element(render)
+// JSX the attribute for JSX is given using camelCase
+// To write it in multiple line wrap it with small brackets ()
+
+const jsxheading = <h1 className="head">Heading using JSX</h1>;  // It is also an object
+
+// React component
+// Class based component - OLD
+// functional component - NEW - first character capital - which returns a react element
+
+// const HeadingComponent = () => {
+//   return <h1>Namaste React functional component</h1>;
+// }
+
+const Title = () => (
+  <h1 className="head" tabIndex="5">
+    Namaste Raact using JSX
+  </h1>
 );
+
+const HeadingComponent = () => (
+  <div id="container">
+    <Title />
+    <h2>{100 + 200}</h2>
+    <h1>Namaste React functional component</h1>
+  </div>
+);
+
+// {} we can write any piece of javascript inside this same way we can include react element inside JSX 
+
+// <Title /> and <Title></Title>  and {Title()} are equivalent
+
+// The above example is for component composition where a component is used inside other component
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+// root.render(jsxheading); // This will replace not append // here jsxheading is a react element
+
+root.render(<HeadingComponent />) // This is syntax for react component
